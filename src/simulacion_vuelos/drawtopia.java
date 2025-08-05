88b5e4996a312b956164e565b3d7712fd84bdafd
Sawi78112 <kelsey.abreu6767@gmail.com>
2025-08-05 16:13:55 -0400

    Feature: Add config file

---
+++ src/simulacion_vuelos/drawtopia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulacion_vuelos;
+import java.util.Random;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author AlejandroMC
+ */
+public class Tormenta {
+    Rectangle rect;
+    Random r;
+    int intensidad;
+    double duracion;
+}
